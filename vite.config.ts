0a9ee5ec268aa2f52e48feb5ca0b069a790745ab
= <alexander.navarro@sofka.com.co>
2024-12-29 22:29:38 -0500

    Se implemento custom hook

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001, // Puerto para desarrollo
+  },
+  preview: {
+    port: 3001, // Puerto para modo de previsualización
+  },
 })
